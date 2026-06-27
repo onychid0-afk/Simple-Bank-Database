58c845f3b45dbfc0bd7fab33a13d6bfd54b983c3
onychid0-afk <onychid0@gmail.com>
2026-06-27 15:14:59 -0600

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,9 @@
+# Withdraw method
+def withdraw(balance, money):
+    balance -= money
+    return balance
+    
+# Deposit method
+def deposit(balance, money):
+    balance += money
+    return balance
